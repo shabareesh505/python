66dc3f1b7439d8f035d04427ab492c3ed698c7ff
shabareesh505 <16shabareesh@gmail.com>
2026-06-08 21:32:11 +0530

    numbers

---
+++ codebasics/exercise/numbers.py
@@ -0,0 +1,21 @@
+#lenght of the foot ball ground
+length=92 #meeter
+width=48.8 #meeter
+print(f"the area of Foot ball ground having {length} length and {width} width is ",round(length*width,2),"Meters.")
+#9 packets of potato chips
+packs=9
+#each packet costs 1.49 dollar
+cost=1.49
+#paid 20 dollars
+paid=20
+print("the dollars we get from shop keeper",paid-(packs*cost))
+#length of the square shaped bathroom is 5.5 feet
+length=5.5
+# area of square is a**2 a is side
+area=length**2
+# tils to replace for square feet cost is 500 rs
+tile_cost=500
+print("the total cost to replace tiles in bathroom is",area*tile_cost)
+# the number to display in binary format is 17
+num=17
+print("the Binary format of 17 is",format(num,"b"))
